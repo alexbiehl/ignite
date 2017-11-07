9b149ccf5f0057b02706c0b16adcdaebcb465358
alexbiehl <alex.biehl@gmail.com>
2017-11-07 09:20:35 +0100

    Selectors

--- src/Ignite/Types/Structs.hs
+++ src/Ignite/Types/Structs.hs
@@ -6,6 +6,8 @@
 {-# LANGUAGE TypeInType #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ConstraintKinds #-}
 module Ignite.Types.Structs where
 
 import GHC.TypeLits
@@ -25,6 +27,9 @@ data Heap root = Heap
 class Storable (Rep a) => Layout a where
   type Rep a
 
+instance Layout Int where
+  type Rep Int = Int
+
 -- | A marker for fields in structure. Indexed by a
 -- type level string.
 data (label :: Symbol) := (value :: Type) = Field
@@ -35,6 +40,23 @@ instance Layout value => Layout (label := value) where
 -- | A struct consists of a sequence of fields.
 newtype Struct (fields :: [Type]) = Struct { getStruct :: Ptr (Struct fields) }
   deriving (Storable)
+
+type family FieldType (field :: Symbol) (fields :: Type) :: Type where
+  FieldType field (Struct (field := value ': fields)) = value
+  FieldType field (Struct (label := value ': fields)) = FieldType field (Struct fields)
+
+class FieldOffset (field :: Symbol) struct where
+  fieldOffset :: Proxy field -> Proxy struct -> Int
+
+instance {-# OVERLAPPING #-} ( Layout value )
+        => FieldOffset field (Struct (field := value ': fields)) where
+  fieldOffset _ _ = 0
+
+instance ( Layout value
+         , FieldOffset field (Struct fields))
+        => FieldOffset field (Struct (label := value ': fields)) where
+  fieldOffset wanted _ =
+    sizeOf (undefined :: Rep value) + fieldOffset wanted (Proxy :: Proxy (Struct fields))
 
 -- | A helper class for determining the size of a 'Struct'
 class StructSize a where
@@ -48,6 +70,27 @@ instance (Layout field, StructSize (Struct fields))
   structSize _ =
     sizeOf (undefined :: Rep field)
       + structSize (Proxy :: Proxy (Struct fields))
+
+data Selector (label :: Symbol) value struct = Selector
+
+instance Layout value => Layout (Selector label value struct) where
+  type Rep (Selector label value struct) = Rep value
+
+selectorOffset
+  :: forall label value struct . FieldOffset label struct
+  =>  Selector label value struct
+  -> Int
+selectorOffset _ = fieldOffset (Proxy :: Proxy label) (Proxy :: Proxy struct)
+
+select
+  :: ( FieldOffset label struct
+     , Layout value
+     )
+  => Selector label value struct
+  -> Struct fields
+  -> IO (Rep value)
+select sel (Struct op) = peekByteOff op offset
+  where offset = selectorOffset sel
 
 -- | A sequence of elements which knows it length.
 newtype Array elem = Array { getArray :: Ptr (Rep elem) }
@@ -71,8 +114,19 @@ type ArrayList elem = Struct '[ "size" := Int, "elems" := Array elem ]
 arrayList :: Proxy (ArrayList elem)
 arrayList = Proxy
 
+arrayListElems :: Layout elem => ArrayList elem -> IO (Array elem)
+arrayListElems =
+  select (Selector :: Selector "elems" (Array elem) (ArrayList elem))
+
+allocArray
+  :: Layout elem
+  => Proxy elem
+  -> Int
+  -> IO (Array elem)
+allocArray _ n = undefined
+
 allocStruct
-  :: (StructSize struct, Layout struct)
+  :: (StructSize struct)
   => Proxy struct
   -> IO struct
 allocStruct proxy = do
