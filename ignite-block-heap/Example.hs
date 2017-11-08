337beb8a27ad2e14895bb204fc727f32e36c2163
Alexander Biehl <abiehl@novomind.com>
2017-11-08 14:46:29 +0100

    Backpackify
    This requires breaking lots of things up into sub-packages

---
+++ ignite-block-heap/Example.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedLabels #-}
+module Ignite.Example where
+
+import Ignite.Layout
+import Ignite.Prim.Array
+import Ignite.Prim.Struct
+import Ignite.BlockHeap
+
+import Control.Monad.Primitive
+import Data.Proxy
+import Data.Foldable
+
+type ArrayList elem = Struct '[ "size" := Int, "elems" := Array elem ]
+
+newArrayList
+  :: forall m elem root . (PrimMonad m, Layout elem)
+  => Heap m root
+  -> Int
+  -> m (ArrayList elem)
+newArrayList heap capacity = do
+  arrayList <- allocStruct heap (Proxy :: Proxy (ArrayList elem))
+  array     <- allocArray heap (Proxy :: Proxy elem) capacity
+  set arrayList #size 0
+  set arrayList #elems array
+  return arrayList
+
+arrayListSize
+  :: forall m elem root . (PrimMonad m)
+  => ArrayList elem
+  -> m Int
+arrayListSize alist = get alist #size
+
+arrayListCapacity
+  :: forall m elem root . (PrimMonad m)
+  => ArrayList elem
+  -> m Int
+arrayListCapacity alist = do
+  arr <- get alist #elems
+  arrayLength arr
+
+arrayListAppend
+  :: forall m elem root . (PrimMonad m, Layout elem)
+  => Heap m root
+  -> ArrayList elem
+  -> elem
+  -> m ()
+arrayListAppend heap alist elem = do
+  size <- arrayListSize alist
+  cap  <- arrayListCapacity alist
+  arr  <- get alist #elems
+
+  if size < cap
+    then do arrayUnsafeWrite arr size elem
+            set alist #size (size + 1)
+    else arrayListResize heap alist >> arrayListAppend heap alist elem
+
+arrayListResize
+  :: forall m root elem . (PrimMonad m, Layout elem)
+  => Heap m root
+  -> ArrayList elem
+  -> m ()
+arrayListResize heap alist = do
+  size <- arrayListSize alist
+  newArr <- allocArray heap (Proxy :: Proxy elem) (2 * size)
+  oldArr <- get alist #elems
+  unsafeArrayCopy oldArr 0 newArr 0 size
+  set alist #elems newArr
+
+arrayListIndex
+  :: forall m elem . (PrimMonad m, Layout elem)
+  => ArrayList elem
+  -> Int
+  -> m elem
+arrayListIndex alist i = do
+  arr  <- get alist #elems
+  arrayUnsafeIndex arr i
+
+test_monotonic :: IO ()
+test_monotonic = withHeap 1024 $ \heap -> do
+  alist <- newArrayList heap 20 :: IO (ArrayList Int)
+
+  for_ [1..25] $ \i ->
+    arrayListAppend heap alist i
+
+  for_ [0..24] $ \i -> do
+    x <- arrayListIndex alist i
+    print x
+
+test :: ArrayList elem -> IO (Array elem)
+test alist = get alist #elems
+
+test1 :: ArrayList Int -> IO (Array Int)
+test1 = test
+
+test2 :: Layout elem => ArrayList elem -> elem -> IO ()
+test2 alist elem = do
+  elems <- get alist #elems
+  arrayUnsafeWrite elems 0 elem
+
+test3 :: ArrayList Int -> Int -> IO ()
+test3 = test2
---
+++ ignite-block-heap/Ignite/Heap.hs
@@ -0,0 +1,143 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedLabels #-}
+module Ignite.Heap (
+    Heap
+  , withHeap
+  , allocStruct
+  , allocArray
+  ) where
+
+import Ignite.Layout
+import Ignite.Prim.Array
+import Ignite.Prim.Struct
+
+import Control.Monad.Primitive
+import Data.IORef
+import Data.Primitive.ByteArray
+import Data.Primitive.MutVar
+import Data.Primitive.Types
+import Data.Proxy
+import Foreign.Marshal.Alloc
+import Foreign.Ptr
+import qualified Foreign.Storable as Storable
+import Data.Word
+
+type BlockLayout =
+  Struct '[ "nextBlock" := Ptr Word8 ]
+
+type Allocator =
+  Struct '[ "nextFree"   := Ptr Word8
+          , "endFree"    := Ptr Word8
+          , "blockSize"  := Int
+          , "firstBlock" := BlockLayout
+          ]
+
+allocBlock :: PrimMonad m => Int -> m (Ptr Word8)
+allocBlock size = do
+  unsafeIOToPrim (mallocBytes (size + blockLayoutSize))
+  where
+    blockLayoutSize = structSize (Proxy :: Proxy BlockLayout)
+
+initAllocator
+  :: forall a m . PrimMonad m
+  => Int
+  -> Ptr a
+  -> m Allocator
+initAllocator blockSize op = do
+  set blockLayout #nextBlock nullPtr
+
+  set allocator #nextFree   firstFreeByte
+  set allocator #endFree    (castPtr op `plusPtr` blockSize)
+  set allocator #blockSize  blockSize
+  set allocator #firstBlock blockLayout
+  return allocator
+  where
+    blockLayoutSize = structSize (Proxy :: Proxy BlockLayout)
+    blockLayout = Struct (castPtr op)
+
+    allocatorSize = structSize (Proxy :: Proxy Allocator)
+    allocator = Struct (castPtr op `plusPtr` blockLayoutSize) :: Allocator
+
+    firstFreeByte = op `plusPtr` (allocatorSize + blockLayoutSize)
+
+withHeap :: PrimMonad m => Int -> (Heap m root -> m a) -> m a
+withHeap blockSize f = do
+  firstBlock <- allocBlock blockSize
+  allocator  <- initAllocator blockSize firstBlock
+  f (Heap allocator)
+
+newtype Heap (m :: * -> *) root = Heap { getHeap :: Allocator }
+
+allocStruct
+  :: forall m struct fields root .
+     ( PrimMonad m
+     , struct ~ Struct fields
+     , StructSize struct
+     )
+  => Heap m root
+  -> Proxy (Struct fields)
+  -> m (Struct fields)
+allocStruct (Heap allocator) _ = do
+  nextFree <- get allocator #nextFree
+  endFree  <- get allocator #endFree
+
+  op <- if nextFree `plusPtr` size >= endFree
+        then do blockSize <- get allocator #blockSize
+                newBlock  <- allocBlock (max size blockSize)
+                let newBlockLayout = Struct (castPtr newBlock) :: BlockLayout
+                Struct firstBlock <- get allocator #firstBlock :: m BlockLayout
+                set newBlockLayout #nextBlock (castPtr firstBlock)
+                set allocator #firstBlock newBlockLayout
+                let freeSpace = newBlock `plusPtr` blockLayoutSize
+                set allocator #nextFree (freeSpace `plusPtr` size)
+                set allocator #endFree (newBlock `plusPtr` blockSize)
+                return freeSpace
+        else do set allocator #nextFree (nextFree `plusPtr` size)
+                return nextFree
+
+  return (Struct (castPtr op))
+  where
+    size = structSize (Proxy :: Proxy struct)
+
+    blockLayoutSize = structSize (Proxy :: Proxy BlockLayout)
+
+allocArray
+  :: forall m elem root .
+     ( PrimMonad m
+     , Layout elem
+     )
+  => Heap m root
+  -> Proxy elem
+  -> Int
+  -> m (Array elem)
+allocArray (Heap allocator) _ n = do
+  nextFree <- get allocator #nextFree
+  endFree  <- get allocator #endFree
+
+  op <- if nextFree `plusPtr` bytes >= endFree
+        then do blockSize <- get allocator #blockSize
+                newBlock  <- allocBlock (max bytes blockSize)
+                let newBlockLayout = Struct (castPtr newBlock) :: BlockLayout
+                Struct firstBlock <- get allocator #firstBlock
+                set newBlockLayout #nextBlock (castPtr firstBlock)
+                set allocator #firstBlock newBlockLayout
+                let freeSpace = newBlock `plusPtr` blockLayoutSize
+                set allocator #nextFree (freeSpace `plusPtr` bytes)
+                set allocator #endFree (newBlock `plusPtr` blockSize)
+                return freeSpace
+        else do set allocator #nextFree (nextFree `plusPtr` bytes)
+                return nextFree
+
+  unsafeIOToPrim (Storable.poke (castPtr op) (n :: Int))
+  return (Array (castPtr op))
+  where
+    bytes = size (Proxy :: Proxy Int) + n * size (Proxy :: Proxy elem)
+    blockLayoutSize = structSize (Proxy :: Proxy BlockLayout)
